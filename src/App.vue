2818256fda6fce570a8c3be1a46a104c315b21bd
Darina Kašprišinová <darina.kasprisinova@gmail.com>
2023-07-18 21:09:29 +0200

    modified:   src/App.vue 	new file:   src/components/Block.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/Results.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Time Meter</h1>
+  <p>Come and play! Click as fast as you can on green box and find out how fast you are! From God Tier to Snail pace... Good luck my friend</p>
+  <button @click="start" :disabled="isPlaying">Play!</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score"></Results>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +43,31 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+
+button {
+  background-color: #333333;
+  color: white;
+  border-radius: 15px;
+  border: none;
+  padding: 15px 30px;
+  box-shadow: inset -5px -5px 10px rgba(0, 0, 0, 0.3),
+  inset 5px 5px 10px rgba(255, 255, 255, 0.3);
+}
+
+button:hover {
+  background-color: #555555;
+  transform: scale(1.04);
+}
+
+button:disabled {
+  opacity: 0.2;
+  cursor: not-allowed;
+  background-color: #999999;
+  color: #666666;
+  box-shadow: none;
+}
+
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>Your reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "God Tier Fast. I salute you!";
+    } else if (this.score < 350) {
+      this.rank = "Rapid Reflexes!!";
+    } else if (this.score < 450){
+      this.rank = "You are fast, but not fastest..";
+    } else if (this.score < 550) {
+      this.rank = "You are slow baby";
+    } else {
+      this.rank = "Snail pace...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
